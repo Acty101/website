dc1fda92fd9993074f8d19d52e71dd422ffa07fe
Acty101 <junkit.work@gmail.com>
2024-05-06 23:16:32 -0500

    added tooltip + modified dropdown to remain

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import NavBar from './navbar';
+import Toast from "react-bootstrap/Toast";
+import Button from "react-bootstrap/Button";
+import NavBar from "./components/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import './App.css';
+import "./App.css";
 
 // const ExampleToast: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 //   const [show, toggleShow] = useState(true);
@@ -20,34 +22,63 @@ import './App.css';
 //     //     <Toast.Body>{children}</Toast.Body>
 //     //   </Toast>
 //     // </>
-    
+
 //   );
 // };
 
-
+/* 
+CONTACT DETAILS:
+*/
+const GITHUB: string = "https://github.com/Acty101";
+const EMAIL: string = "junkit@umich.edu";
+const LINKEDIN: string = "https://www.linkedin.com/in/junkitlim/";
+const NUMBER: string = "(734) 450-5507";
 
 function App() {
   const [img, setImg] = useState("./profile.jpg");
   return (
     <>
-      <NavBar></NavBar>;
-      <div className="profile-pic-container">
-          {img && <img className="profile-pic" src={img} width="30" height="30" onError={(e) => {setImg("")}} style={{ marginRight: '10px' }}/>}
-      </div>
-      Jun Kit Lim         
+      <NavBar
+        github={GITHUB}
+        email={EMAIL}
+        linkedin={LINKEDIN}
+        number={NUMBER}
+      />
+      <Container>
+        <Row>
+          <Col>
+            <div className="profile-pic-container">
+              {img && (
+                <img
+                  className="profile-pic"
+                  src={img}
+                  width="30"
+                  height="30"
+                  onError={(e) => {
+                    setImg("");
+                  }}
+                  style={{ marginRight: "10px" }}
+                />
+              )}
+            </div>
+            Jun Kit Lim
+          </Col>
+          <Col xs={9}>About Me</Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
-  // <Container className="p-3">
-  //   <Container className="p-5 mb-4 bg-light rounded-3">
-  //     <h1 className="header">Welcome To React-Bootstrap</h1>
-  //     <ExampleToast>
-  //       We now have Toasts
-  //       <span role="img" aria-label="tada">
-  //         🎉
-  //       </span>
-  //     </ExampleToast>
-  //   </Container>
-  // </Container>
+// <Container className="p-3">
+//   <Container className="p-5 mb-4 bg-light rounded-3">
+//     <h1 className="header">Welcome To React-Bootstrap</h1>
+//     <ExampleToast>
+//       We now have Toasts
+//       <span role="img" aria-label="tada">
+//         🎉
+//       </span>
+//     </ExampleToast>
+//   </Container>
+// </Container>
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Tooltip } from "react-tooltip";
+
+interface IconWordProps {
+  imgUrl: string;
+  text: string;
+  clickHandler?: () => void;
+}
+
+interface TooltipIconWordProps extends IconWordProps {
+  id: string;
+  msg: string;
+  hideHandler?: () => void;
+}
+
+interface ContantLinksProps {
+  github: string;
+  linkedin: string;
+  number: string;
+  email: string;
+}
+
+function IconWord({ imgUrl, text, clickHandler }: IconWordProps) {
+  return (
+    <div onClick={clickHandler}>
+      <img
+        src={imgUrl}
+        width="25"
+        height="25"
+        style={{ marginRight: "10px" }}
+      />
+      {text}
+    </div>
+  );
+}
+
+function TooltipCopyIconWord({
+  id,
+  imgUrl,
+  text,
+  msg,
+  clickHandler,
+  hideHandler,
+}: TooltipIconWordProps) {
+  return (
+    <>
+      <div data-tooltip-id={id} data-tooltip-content={msg}>
+        <div onClick={clickHandler}>
+          <img
+            src={imgUrl}
+            width="25"
+            height="25"
+            style={{ marginRight: "10px" }}
+          />
+          {text}
+        </div>
+      </div>
+      <Tooltip id={id} afterHide={hideHandler} place="right-end" />
+    </>
+  );
+}
+
+function NavBar({ github, linkedin, number, email }: ContantLinksProps) {
+  const COPYMSG = "Copy to clipboard?";
+  const [phoneMsg, setPhoneMsg] = useState(COPYMSG);
+  const [emailMsg, setEmailMsg] = useState(COPYMSG);
+  const resetPhoneMsg = () => {
+    setPhoneMsg(COPYMSG);
+  };
+  const resetEmailMsg = () => {
+    setEmailMsg(COPYMSG);
+  };
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+      <Container className="justify-content-end">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Navbar.Text>
+            <Nav className="me-auto">
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <NavDropdown
+                title="Contact"
+                id="basic-nav-dropdown"
+                autoClose="outside"
+              >
+                <NavDropdown.Item>
+                  <TooltipCopyIconWord
+                    id="tooltip-anchor-phone"
+                    imgUrl="./phone.png"
+                    text={number}
+                    clickHandler={() => {
+                      navigator.clipboard.writeText(number);
+                      setPhoneMsg("Copied!");
+                    }}
+                    msg={phoneMsg}
+                    hideHandler={resetPhoneMsg}
+                  />
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <TooltipCopyIconWord
+                    id="tooltip-anchor-email"
+                    imgUrl="./gmail.svg"
+                    text={email}
+                    clickHandler={() => {
+                      navigator.clipboard.writeText(email);
+                      setEmailMsg("Copied!");
+                    }}
+                    msg={emailMsg}
+                    hideHandler={resetEmailMsg}
+                  />
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Links" id="basic-nav-dropdown">
+                <NavDropdown.Item href={github}>
+                  <IconWord imgUrl="./github-mark-white.svg" text="GitHub" />
+                </NavDropdown.Item>
+                <NavDropdown.Item href={linkedin}>
+                  <IconWord imgUrl="./linkedin_icon.svg" text="LinkedIn" />
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
